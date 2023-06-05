56150e58a4fef00729dfd91be15a71bd6c0fcb64
Dani Nugraha <dani.hmng@gmail.com>
2023-06-05 23:08:11 +0700

    added Send as super-trait to Database trait

--- src/database/database.rs
+++ src/database/database.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 
 use crate::structs::Chapter;
 
-pub trait Database {
+pub trait Database: Send {
     fn initialize_database(&self) -> Result<()>;
     fn save_chapters(&self, chapters: &[Chapter]) -> Result<()>;
 }
