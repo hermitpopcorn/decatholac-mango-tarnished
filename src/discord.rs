b4c3c0a2d45e75d4a273c85e916a9d8de98804f8
Dani Nugraha <dani.hmng@gmail.com>
2023-06-14 18:36:15 +0700

    updated discord implementation

--- src/discord.rs
+++ src/discord.rs
@@ -2,13 +2,17 @@ use std::sync::Arc;
 
 use anyhow::{anyhow, bail, Result};
 use crossbeam::channel::Sender;
-use poise::{serenity_prelude as serenity, Framework, FrameworkBuilder};
+use poise::{
+    serenity_prelude::{self as serenity, ChannelId, Http},
+    Framework, FrameworkBuilder,
+};
 use tokio::sync::Mutex;
 use toml::Value as TomlValue;
 
-use crate::{database::database::Database, log, CoreMessage};
+use crate::{database::database::Database, log, structs::Chapter, CoreMessage};
 struct Data {
     sender: Sender<CoreMessage>,
+    database: Arc<Mutex<dyn Database>>,
 }
 type PoiseError = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, PoiseError>;
@@ -36,16 +40,25 @@ pub async fn connect_discord(
 
     let framework: FrameworkBuilder<Data, PoiseError> = Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![trigger_start_gofer()],
+            commands: vec![trigger_start_gofer(), set_as_feed_channel()],
             ..Default::default()
         })
         .token(token)
         .intents(serenity::GatewayIntents::non_privileged())
-        .setup(|ctx, _ready, framework| {
+        .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 log!("Connected to Discord.");
-                Ok(Data { sender: sender })
+
+                // Send Discord API back to core control
+                log!("Sending Discord API back to core control...");
+                let discord_http = ctx.http.clone();
+                sender.send(CoreMessage::TransferDiscordHttp(discord_http))?;
+
+                Ok(Data {
+                    sender: sender,
+                    database: database,
+                })
             })
         });
 
@@ -59,5 +72,45 @@ pub async fn connect_discord(
 async fn trigger_start_gofer(ctx: Context<'_>) -> Result<(), PoiseError> {
     let _ = ctx.data().sender.send(CoreMessage::StartGofer)?;
     ctx.say("Fetching process triggered.").await?;
+    Ok(())
+}
+
+/// Set current channel as the feed channel. You must have channel management permissions to do this.
+#[poise::command(
+    slash_command,
+    ephemeral,
+    rename = "set-as-feed-channel",
+    default_member_permissions = "MANAGE_CHANNELS"
+)]
+async fn set_as_feed_channel(ctx: Context<'_>) -> Result<(), PoiseError> {
+    let guild_id = ctx.guild_id();
+    if guild_id.is_none() {
+        ctx.say("Could not get Server ID.").await?;
+        return Ok(());
+    }
+    let guild_id = guild_id.unwrap();
+    let channel_id = ctx.channel_id();
+
+    let db = ctx.data().database.lock().await;
+    db.set_feed_channel(
+        guild_id.to_string().as_str(),
+        channel_id.to_string().as_str(),
+    )?;
+
+    ctx.say("This channel has been set as the feed channel.")
+        .await?;
+    Ok(())
+}
+
+pub async fn send_chapters(http: &Http, channel: ChannelId, chapters: Vec<Chapter>) -> Result<()> {
+    for chapter in chapters {
+        let title = format!("[{}] {}", chapter.manga, chapter.title);
+        channel
+            .send_message(http, |m| {
+                m.embed(|e| e.timestamp(chapter.date).title(title).url(chapter.url))
+            })
+            .await?;
+    }
+
     Ok(())
 }
